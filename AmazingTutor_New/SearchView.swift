384a917fdd6cda497b5121a1b0d954b32340456f
JulyYang <julyba3101@gmail.com>
2016-05-02 12:24:58 +0800

    建xib檔

---
+++ AmazingTutor_New/SearchView.swift
@@ -0,0 +1,29 @@
+//
+//  SearchView.swift
+//  AmazingTutor_New
+//
+//  Created by apple on 2016/5/2.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+class SearchView: UIView {
+
+    @IBOutlet weak var SearchTitle1: UILabel!
+    @IBOutlet weak var SearchContainer1: UIView!
+    @IBOutlet weak var SearchTitle2: UILabel!
+    @IBOutlet weak var SearchContainer2: UIView!
+    @IBOutlet weak var PriceTitle: UILabel!
+    @IBOutlet weak var PriceSlider: UISlider!
+    
+    @IBAction func SendToSearchBar(sender: AnyObject) {
+    }
+    
+    
+    
+    
+    
+    
+    
+}
